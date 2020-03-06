943242353db97609ab45df3ac12970aae33609cc
Drewert <58240058+Drewert@users.noreply.github.com>
2020-03-05 19:30:12 -0600

    lab complete

--- BlynkStarter/BlynkStarter.ino
+++ BlynkStarter/BlynkStarter.ino
@@ -31,6 +31,7 @@
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
+#define LED 2
 
 
 #include <WiFi.h>
@@ -39,24 +40,91 @@
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "Your token here";
+char auth[] = "yRsmw7UxeHjOGrtUr0zOfA-rE34fd6L0";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 // The EE IOT network is hidden. You might not be able to see it.
 // But you should be able to connect with these credentials. 
-char ssid[32] = "EE-IOT-Platform-02";
-char pass[32] = "g!TyA>hR2JTy";
+char ssid[32] = "Drew";
+char pass[32] = "12345678";
+const int ledChannel = 0;
+const int freq = 5000;
+const int resolution = 10;
+int ledState = 0;
+int brightness = 0;
+BlynkTimer timer;
+int timeCounter = 0;
+String message = "";
+
+
+/*
+ * V0 = button
+ * V1 = slider
+ * V2 = display
+ * V3 = terminal
+ */
 
 void setup()
 {
   // Serial Monitor
   Serial.begin(115200);
   Blynk.begin(auth, ssid, pass);
+  timer.setInterval(10L, myTimerEvent);
+  pinMode(LED, OUTPUT);
+  ledcSetup(ledChannel, freq, resolution);
+  ledcAttachPin(LED, ledChannel);
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run();
 }
 
+void myTimerEvent()
+{
+  if(timeCounter == 100)
+  {
+    Blynk.virtualWrite(V2, millis() / 1000);
+    timeCounter = 0;
+  }
+  char character;
+  while(Serial.available())
+  {
+    character = Serial.read();
+    message.concat(character);
+  }
+
+  if(message != "")
+  {
+    Blynk.virtualWrite(V3, message);
+    message = "";
+  }
+
+  timeCounter ++;
+}
+
+BLYNK_WRITE(V0)//button
+{
+  int pinValue = param.asInt();
+  if(pinValue == 0)
+  {
+    ledState = 0;
+    ledcWrite(ledChannel, 0);
+  }
+  else
+  {
+    ledState = 1;
+    ledcWrite(ledChannel, brightness);
+  }
+}
+BLYNK_WRITE(V1)//slider
+{
+  int pinValue = param.asInt();
+  brightness = pinValue;
+  if(ledState == 1)
+  {
+    ledcWrite(ledChannel, brightness);
+  }
+}
--- Report.md
+++ Report.md
@@ -1,6 +1,6 @@
-Name:
+Name:Andrew Martin
 
-EID:
+EID:adm4799
 
 Team Number:
 
@@ -8,36 +8,36 @@ Team Number:
 
 1. What is the purpose of an IP address?
 
-    answer
+	Gives unique name to a machine connected to a network- know who is sending/recieving 
 
 2. What is a DNS? What are the benefits of using domain names instead of IP addresses?
 
-    answer
+    Domain Name System matches a domain to an IP, domain names are more rememberable and human orientated 
 
 3. What is the difference between a static IP and a dynamic IP?
 
-    answer
+	Static IP never changes, dynamic IP will change- especially when connecting to a new server.
 
 4. What is the tradeoff between UDP and TCP protocols?
 
-    answer
+    TCP is more reliable, UDP is faster 
 
 5. Why can't we use the delay function with Blynk?
 
-    answer
+    Becuase the delay function would prevent the other routines to run- becuase the processing would be commited to the delay function
 
 6. What does it mean for a function to be "Blocking"?
 
-    answer
+    oppertunity cost of running a function- prevents other functions
 
 7. Why are interrupts useful for writing Non-Blocking code?
 
-    answer
+	Becuase it doesnt waste processing on checking if an event has occured, which would block other functions
 
 8. What is the difference between interface and implementation? Why is it important?
-
-   answer
+	Interface is how or when its being use, implementation is what it is doing. Allows for abstraction and effeciency of code. (Concept of subroutine)
+   
 
 9. Screenshot of your Blynk App:
 
-    ![your image here->](img/placeholder.png)
+    ![your image here->](img/Bapp.png)
